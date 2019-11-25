8e9a75b8c767df0df79d2ca46109098d77451cd7
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-11-24 19:59:29 -0500

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,19 +12,19 @@ export class Contact extends Component {
 
                         <form action="mailto:sjones.arise@gmail.com" method="post" enctype="text/plain">
                             <div className='row'>
-                                <div className='col-6'>
+                                <div className='col-lg-6 col-sm-12'>
 
                                     <input placeholder='Name' className="form-control" type="email" name="name"></input>
 
                                 </div>
 
-                                <div className='col-6'>
+                                <div className='col-lg-6 col-sm-12'>
 
                                     <input placeholder='Email Address' className="form-control" type="text" name="mail"></input>
                                 </div>
                             </div>
                             <div className='row'>
-                                <div className='col-12'>
+                                <div className='col-lg-6 col-sm-12'>
 
                                     <textarea name="comment" rows="4" cols="50">
                                         Contact Me!
@@ -32,7 +32,7 @@ export class Contact extends Component {
                                 </div>
                             </div>
                             <div className='row'>
-                                <div className='col-6'>
+                                <div className='col-lg-6 col-sm-12'>
                                     <button className='send btn btn-primary' type="submit" value="Send">Send</button>
                                     <button className='reset btn btn-primary' type="reset" value="Reset">Reset</button>
                                 </div>
